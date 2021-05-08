9eb8477b4b4d1b4b535e217191ad36cfbe6953e7
AntonioMontoyaaA <70233278+AntonioMontoyaaA@users.noreply.github.com>
2021-05-07 19:52:27 -0500

    Tercer commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps bitches!! Con un primer Commit!!!'));
+app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps es AWSome'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
