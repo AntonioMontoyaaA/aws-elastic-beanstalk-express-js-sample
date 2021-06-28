4f278efe588b082c02cde21df486e79990192ad4
AntonioMontoyaaA <70233278+AntonioMontoyaaA@users.noreply.github.com>
2021-06-28 16:56:19 -0500

    Pruebas con el TeamEva

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps <h1>Primeros pasos al DevOps</h1>'));
+app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps <h1>Primeros pasos al DevOps</h1> <h2>Ejemplo con el TeamEva</h2>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
