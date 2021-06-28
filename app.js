1a2e11e2afc5e7b4e5e49dd749c0d95871f9d9b1
AntonioMontoyaaA <70233278+AntonioMontoyaaA@users.noreply.github.com>
2021-06-28 14:32:24 -0500

    Cambios en texto

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps'));
+app.get('/', (req, res) => res.send('Hola Mundo ejemplo DevOps <h1>Primeros pasos al DevOps</h1>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
